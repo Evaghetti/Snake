ef3cae68e88bf9c1ec8baf8d1a91e6173d083813
Evaghetti <1evaghetti@gmail.com>
2018-09-29 18:26:22 -0300

    Mudanças no código pra espawnar maçãs

--- Source/PlayState.cpp
+++ Source/PlayState.cpp
@@ -24,16 +24,24 @@ void PlayState::update() {
     const float deltaTime = getDeltaTime();
 
     player.update(deltaTime);
-    for (auto& apple : apples) {
+    for (auto it = apples.begin(); it != apples.end(); it++) {
+        Apple& apple = *it;
+
         if (player.comeu(apple)) {
             player.aumentarCorpo();
 
             const auto& corpo = player.getCorpo();
+            int cont = 0;
             do {
                 apple.respawn();
             } while(player.comeu(apple) || std::any_of(corpo.begin(), corpo.end(), [&](auto a) {
-                return a.colidiuCom(apple);
+                return a.colidiuCom(apple) && ++cont < 640 * 480;
             }));
+
+            if (cont == 640 * 480) {
+                apples.erase(it);
+                it--;                
+            }
         }
     }
 }
